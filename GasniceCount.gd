97b221d5f88afaecb3a5546835beb0a2beae58e0
patrykduda2005 <patrykduda7@gmail.com>
2024-02-28 10:15:36 +0100

    click -> gasnica

---
+++ GasniceCount.gd
@@ -0,0 +1,13 @@
+extends RichTextLabel
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var globals = get_node("..")
+	self.text = "Gasnice: " + str(globals.gasnice)
+	pass
---
+++ Globale.gd
@@ -0,0 +1,12 @@
+extends Node2D
+@export var gasnice = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print_debug(self.gasnice)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
